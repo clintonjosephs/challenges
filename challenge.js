ab655a995c47ada551255e7b029706f6938cf80c
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-09 09:03:40 +0100

    resolved mob callenge

--- challenge.js
+++ challenge.js
@@ -1,6 +1,27 @@
 function balancedBrackets(string) {
-  // your code here
+  let stack = [];
+  let match = {
+    "{":"}",
+    "[":"]",
+    "(":")"
+  };
+
+  for (i = 0; i < string.length; i++) {
+      const pickedLetter = string.charAt(i);
+      if (pickedLetter === "(" || pickedLetter === "[" || pickedLetter === "{") {
+        stack.push(pickedLetter);
+      } else if (pickedLetter === ")" || pickedLetter === "]" || pickedLetter === "}") {
+           const lastItem = stack[stack.length - 1];
+          if (match[lastItem] === pickedLetter) {
+            stack.pop();
+          }
+      }
+     
+  }
+  return stack.length > 0 ? false : true;
 }
+
+
 
 console.log(balancedBrackets('(hello)[world]'))
 // => true
