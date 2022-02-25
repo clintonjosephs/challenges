8b440a05cacac513d48d0de7b57513fccc50040a
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-25 08:24:48 +0100

    solved mob-programming challenge

--- challenge.js
+++ challenge.js
@@ -1,6 +1,8 @@
-module.exports = function sum(number) {
+function sum(number) {
   // write your code here
+    // write your code here
+    if (number === 0) {
+      return number;
+    }
+    return number + sum(number - 1);
 }
-
-console.log(sum(4))
-console.log(sum(10))
